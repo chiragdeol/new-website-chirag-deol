0d712fd0b529fbc4bc15df779cebacd720195896
Chirag <cdeol48@gmail.com>
2026-05-30 20:29:43 +0530

    Remove excessive padding at bottom of home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,7 +47,7 @@ export default function HomePage() {
   }, []);
 
   return (
-    <main className="relative pb-40 sm:pb-44">
+    <main className="relative">
       {/* Loader overlay - fades out */}
       <div
         id="page-loader"
